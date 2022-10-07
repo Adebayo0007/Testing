b1928a2a05017599266c9d90e8ba659ec6668ad9
unknown <tijaniadebayoabdllahi@gmail.com>
2022-10-07 10:38:41 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Write($"enter your name : ");
+            string user = Console.ReadLine();
+           UserName(user);
+        }
+        public static void UserName(string name)
+        {
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
